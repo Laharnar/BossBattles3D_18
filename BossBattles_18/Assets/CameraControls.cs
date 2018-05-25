19f4eea8f2a2674eebc278f0fbc8cfa0bb609052
Laharnar <marko.laharnar@gmail.com>
2018-05-25 14:27:49 +0200

    simple wasd no rotation player rotating camera

---
+++ BossBattles_18/Assets/CameraControls.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour {
+    public bool move = true;
+    public Vector3 offset;
+    public Transform target;
+
+    public float rotSpeed = 5f;
+    public Transform rotationTarget;
+    Vector3 rotOffset;
+	// Use this for initialization
+	void Start () {
+        offset = Camera.main.transform.position - transform.position;
+    }
+	
+	// Update is called once per frame
+	public void Move () {
+        if (move) {
+            Vector3 targetPos = target.position + offset+rotOffset;
+            Camera.main.transform.position = targetPos;
+        }
+    }
+
+    internal void Rotate(float rot) {
+        Vector3 pos = Camera.main.transform.position;
+        Camera.main.transform.RotateAround(rotationTarget.position, Vector3.up, rot*rotSpeed*Time.deltaTime);
+        rotOffset += Camera.main.transform.position - pos;
+    }
+}
---
+++ BossBattles_18/Assets/PlayerControls.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+
+    public float speed = 5f;
+
+    public Rigidbody rig;
+
+    public CameraControls cam;
+
+    // Use this for initialization
+    void Start () {
+        rig = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float rot = Input.GetAxis("Mouse X");
+
+        Vector3 dir = new Vector3(h, 0, v);
+        Move(dir);
+
+        cam.Move();
+        cam.Rotate(rot);
+	}
+
+    private void Move(Vector3 dir) {
+        rig.MovePosition(transform.position+transform.TransformDirection(dir) * speed * Time.deltaTime);
+    }
+}
