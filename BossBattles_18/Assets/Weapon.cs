3fdd6e96d717a3f7df7fef18f750741c45e42e7c
Laharnar <marko.laharnar@gmail.com>
2018-06-03 14:56:01 +0200

    melle weapons and hp

---
+++ BossBattles_18/Assets/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+public class Weapon:MonoBehaviour {
+
+    public int baseDamage = 5;
+    public bool attackPlaying = true;
+    List<Transform> hitHistory = new List<Transform>();
+
+    private void OnTriggerEnter(Collider collision) {
+        Debug.Log("asd");
+        if (attackPlaying) {
+            // don't allow multiple hits per attack with edge case collisions.
+            if (hitHistory.Contains(collision.transform.root)) {
+                return;
+            }
+            //hitHistory.Add(collision.transform.root);
+            UnitController uc = collision.transform.root.GetComponentInChildren<UnitController>();
+            uc.TryDamage(this);
+        }
+    }
+
+    void OnAttackEnd() {
+        attackPlaying = false;
+        hitHistory.Clear();
+    }
+}
