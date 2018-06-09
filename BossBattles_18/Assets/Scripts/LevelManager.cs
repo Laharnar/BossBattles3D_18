cfa775492490ca023c58ef3db3728c16ec324bfc
Laharnar <marko.laharnar@gmail.com>
2018-06-09 14:56:44 +0200

    scripts moved to separate folder added main menu added level loading manager

---
+++ BossBattles_18/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelManager:MonoBehaviour {
+    static LevelManager _m;
+
+    public static LevelManager m {
+        get {
+            if (_m == null) {
+                _m = new GameObject().AddComponent<LevelManager>();
+            }
+            return _m;
+        }
+    }
+
+    public void LoadScene(string name) {
+        SceneManager.LoadScene(name);
+    }
+}
